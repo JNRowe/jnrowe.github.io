cf953fbe76fc56060c013d41a35d246dc02db802
James Rowe <jnrowe@gmail.com>
2011-03-25 08:38:08 +0000

    Remove unused vim-configs documentation link.

--- conf.py
+++ conf.py
@@ -246,7 +246,7 @@ man_pages = [
 
 intersphinx_mapping = {}
 for proj in ['blanco', 'bleeter', 'bwatch', 'cupage', 'fixes-overlay',
-               'misc-overlay', 'versionah', 'vim-configs']:
+               'misc-overlay', 'versionah']:
     # IDs must be alphanumeric
     proj_id = filter(lambda s: s.isalnum(), proj)
     intersphinx_mapping[proj_id] = ("http://jnrowe.github.com/%s/" % proj,
