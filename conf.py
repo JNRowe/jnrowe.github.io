ba7ffc9f0ab2b943a898a9d294ebd5ddb822ad55
James Rowe <jnrowe@gmail.com>
2017-09-09 09:14:47 +0100

    [Tr] Bump copyright years

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ source_suffix = '.rst'
 
 # General information about the project.
 project = u'JNRowe'
-copyright = u'2009-2015, James Rowe'
+copyright = u'2009-2017, James Rowe'
 
 version = '0.1'
 release = '0.1.0'
