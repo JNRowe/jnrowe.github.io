69f8f88f0c09f5ab5a867c09251f085e43652e51
James Rowe <jnrowe@gmail.com>
2017-11-07 06:18:23 +0000

    Sync Sphinx config across projects

--- conf.py
+++ conf.py
@@ -1,32 +1,39 @@
-# coding=utf-8
+#
+"""conf - Sphinx configuration information."""
 
 import os
 import sys
 
+from contextlib import suppress
+from subprocess import CalledProcessError, PIPE, run
+
 import sphinx_rtd_theme
 
-sys.path.extend([os.path.pardir, os.path.curdir, '/home/jay/Projects/feed'])
+sys.path.extend([os.path.curdir, os.path.pardir])
 
+# General configuration {{{
 extensions = \
-    ['sphinx.ext.%s' % ext for ext in ['extlinks', 'githubpages',
-                                       'intersphinx', ]] + \
+    ['sphinx.ext.%s' % ext
+     for ext in ['extlinks', 'githubpages', 'intersphinx', ]] + \
     ['sphinxcontrib.%s' % ext for ext in []] + \
     ['ext.%s' % ext for ext in ['jinja', ]] + \
-    ['%s' % ext for ext in ['feed', ]]
+    ['feed', ]
 
-# Only activate spelling, if it is installed.  It is not required in the
-# general case and we don't have the granularity to describe this in a clean
+# Only activate spelling if it is installed.  It is not required in the
+# general case and we don’t have the granularity to describe this in a clean
 # way
 try:
-    from sphinxcontrib import spelling  # NOQA
+    from sphinxcontrib import spelling  # NOQA: F401
 except ImportError:
     pass
 else:
     extensions.append('sphinxcontrib.spelling')
 
 master_doc = 'index'
-source_suffix = '.rst'
-html_copy_source = False
+
+exclude_patterns = ['README.rst', '.build', 'draft']
+
+templates_path = ['.templates', ]
 
 rst_epilog = """
 .. |CLA| replace:: :abbr:`CLA (Contributor License Agreement)`
@@ -37,45 +44,70 @@ rst_epilog = """
 .. |XML| replace:: :abbr:`XML (Extensible Markup Language)`
 """
 
-# General information about the project.
-project = u'JNRowe'
-copyright = u'2009-2017, James Rowe'
+default_role = 'any'
+
+needs_sphinx = '1.6'
+
+nitpicky = True
+# }}}
+
+# Project information {{{
+project = 'JNRowe'
+copyright = '2009-2017  James Rowe'
 
 version = '0.1'
 release = '0.1.0'
 
-exclude_patterns = ['README.rst', '.build', 'draft']
+trim_footnote_reference_space = True
+# }}}
 
+# Options for HTML output {{{
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), ]
 
-html_context = {'feed_link': True}
 html_title = 'JNRowe'
+
+html_context = {'feed_link': True}
 html_favicon = '.static/icon.ico'
-
 html_static_path = ['.static', ]
+html_extra_path = ['.well-known/', ]
 
-templates_path = ['.templates', ]
+with suppress(CalledProcessError):
+    proc = run(['git', 'log', "--pretty=format:'%ad [%h]'", '--date=short',
+                '-n1'],
+               stdout=PIPE)
+    html_last_updated_fmt = proc.stdout.decode()
 
+html_copy_source = False
+
+html_experimental_html5_writer = True
+# }}}
+
+# extlinks extension settings {{{
 extlinks = {
     'pypi': ('https://pypi.python.org/pypi/%s', ''),
 }
+# }}}
 
+# intersphinx extension settings {{{
 intersphinx_mapping = {
     'python': ('http://docs.python.org/', os.getenv('SPHINX_PYTHON_OBJECTS')),
 }
 for proj in ['blanco', 'bleeter', 'bwatch', 'cupage', 'jnrowe-fixes',
              'pyisbn', 'upoints', 'versionah', 'vim-jnrowe']:
     # IDs must be alphanumeric
-    proj_id = filter(lambda s: s.isalnum(), proj)
-    intersphinx_mapping[proj_id] = ('http://jnrowe.github.io/%s/' % proj,
+    proj_id = ''.join(filter(str.isalnum, proj))
+    intersphinx_mapping[proj_id] = ('https://jnrowe.github.io/%s/' % proj,
                                     'objects/%s.inv' % proj)
 
 for proj in ['jnrowe-misc', ]:
     # IDs must be alphanumeric
-    proj_id = filter(lambda s: s.isalnum(), proj) + 'docs'
-    intersphinx_mapping[proj_id] = ('http://jnrowe.github.io/%s-docs/' % proj,
+    proj_id = ''.join(filter(str.isalnum, proj)) + 'docs'
+    intersphinx_mapping[proj_id] = ('https://jnrowe.github.io/%s-docs/' % proj,
                                     'objects/%s.inv' % proj)
+# }}}
 
+# spelling extension settings {{{
 spelling_lang = 'en_GB'
 spelling_word_list_filename = 'wordlist.txt'
+# }}}
