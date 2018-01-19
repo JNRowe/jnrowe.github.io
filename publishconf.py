ba210b2398f3fcf64db30675ab6069f1df90ad15
James Rowe <jnrowe@gmail.com>
2018-01-19 06:24:31 +0000

    Customise pelican config

--- publishconf.py
+++ publishconf.py
@@ -1,24 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
-import os
 import sys
-sys.path.append(os.curdir)
+
+sys.path.append('.')
+
 from pelicanconf import *
 
-SITEURL = 'http://jnrowe.github.io'
-RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# Basic settings {{{
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
+SITEURL = 'https://jnrowe.github.io'
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+RELATIVE_URLS = False
+
+# }}}
+
+# Feed settings {{{
+
+FEED_DOMAIN = SITEURL
+
+FEED_ALL_ATOM = 'updates.atom'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+FEED_MAX_ITEMS = 15
+
+# }}}
